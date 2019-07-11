064b31d26785aa11361b4897cacd21f456942773
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2019-07-11 20:03:17 +0900

    update ready workshop

--- src/app.js
+++ src/app.js
@@ -32,6 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 
+
 // Set up Plugins and providers
 app.configure(express.rest());
 app.configure(socketio({}));
--- src/authentication.js
+++ src/authentication.js
@@ -5,18 +5,26 @@ const oauth2 = require('@feathersjs/authentication-oauth2');
 const GoogleStrategy = require('passport-google-oauth20');
 const FacebookStrategy = require('passport-facebook');
 const GithubStrategy = require('passport-github');
+const EmailFirstOAuth2Verifier = require('./verifiers/verifier.google');
+const makeHandler = require('./oauth-handler');
 
 module.exports = function (app) {
   const config = app.get('authentication');
+
+  // Create a handler by passing the `app` object.
+  const handler = makeHandler(app);
 
   // Set up authentication with the secret
   app.configure(authentication(config));
   app.configure(jwt());
   app.configure(local());
 
+
   app.configure(oauth2(Object.assign({
     name: 'google',
-    Strategy: GoogleStrategy
+    Strategy: GoogleStrategy,
+    Verifier: EmailFirstOAuth2Verifier,
+    emailField: 'email'
   }, config.google)));
 
   app.configure(oauth2(Object.assign({
@@ -26,7 +34,8 @@ module.exports = function (app) {
 
   app.configure(oauth2(Object.assign({
     name: 'github',
-    Strategy: GithubStrategy
+    Strategy: GithubStrategy,
+    handler: handler(config.github.successRedirect)
   }, config.github)));
 
   // The `authentication` service is used to create a JWT.
